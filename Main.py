72808add6ea12460227d24d704464dda14b7f52e
shifty189 <shifty189@gmail.com>
2020-06-14 10:27:28 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,19 +6,12 @@ import tkinter as tk
 # import re
 
 def wake():
-    #regextext = "^([0-9A-F]{2}[:-]){5}([0-9A-F]{2})$"
-    #print(re.match(regextext, mac_text.get()))
     print(mac_text.get())
     try:
         send_magic_packet(mac_text.get())
     except ValueError:
         messagebox.showerror(message="Incorrect MAC address entered")
-    # if re.match(regextext, mac_text.get()):
-    #     send_magic_packet(mac_text.get())
-    #     messagebox.showinfo(message='Magic Packet sent to ' + mac_text.get())
-    # else:
-    #     messagebox.showerror(title= "Enter Mac", message="Must enter MAC address of target device\n example: 00:11:22:33:44:55")
-
+    
 main = tk.Tk(screenName="Puter Waker", baseName="Waker", className="Waker")
 main.title('Puter Waker 0.3')
 
