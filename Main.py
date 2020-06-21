2c25c130de8e81a956d2d84b7b46acb7631de744
shifty189 <shifty189@gmail.com>
2020-06-21 11:14:50 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,14 +1,7 @@
-# reference https://pypi.org/project/wakeonlan/
-
 from wakeonlan import send_magic_packet
 from tkinter import messagebox
 import tkinter as tk
 import os
-# import re
-
-
-def get_index(x):
-    return x
 
 
 def arp():
@@ -25,7 +18,6 @@ def arp():
     for i, a in enumerate(arpData):
         if i > 0:
             var.append(a)
-    # print(var[7])
     for i, data in enumerate(var):
         try:
             start = data.find("-") - 2
@@ -34,15 +26,13 @@ def arp():
             macs.append(' ')
     for i, data in enumerate(macs):
         d = data
-        # lambda: num = i
-        # lambda num: num
         if i > 0:
             if i < 17:
                 ArpLabels.append(tk.Label(arp_window, text=data).grid(row=i, column=0))
                 ArpButton.append(tk.Button(arp_window, text="Wake! " + str(i),
                                            command=(lambda num = i:lambda:wake(macs[num]))()
                                            ).grid(row=i, column=1))
-                print(d + " Sent")
+
             else:
                 ArpLabels.append(tk.Label(arp_window, text=data).grid(row=i - 17, column=3))
                 ArpButton.append(tk.Button(arp_window, text="Wake!",
@@ -50,20 +40,9 @@ def arp():
                                            ).grid(row=i - 17, column=4))
         else:
             ArpLabels.append(tk.Label(arp_window, text=data).grid(row=i, column=0))
-    # test = var[5].find("-") - 2
-    # print(var[5][test:test + 18:1])
-        # if i < 2:
-        #     break
-        # else:
-        #     print(i)
-    #arp_display = tk.Label(arp_window, text=arpData)
-    #arp_display.grid(row=0, column=0)
-    #arpData[2] is the legend, 3 starts the clients
-    # print(arpData[2])
 
 
 def wake(x):
-    print(x)
     try:
         send_magic_packet(x)
         messagebox.showinfo(title="Wake sent", message="WOL sent to " + str(x))
@@ -78,14 +57,8 @@ main.title('Puter Waker 1.1')
 
 describe_lab = tk.Label(main, text="Enter the MAC address of the computer you want to wake").grid(row=0, columnspan=2)
 mac_text = tk.StringVar()
-# mac_text.set(" ")
 mac_entry = tk.Label(main, text="Mac Address:").grid(row=1, column=0)
 mac_input = tk.Entry(main, textvariable=mac_text).grid(row=1, column=1)
-#or_label = tk.Label(main, text="or").grid(row=2, columnspan=2)
-#ip_text = tk.StringVar()
-#ip_text.set("Not working just yet")
-#ip_entry = tk.Label(main, text="IP Address", textvariable= ip_text).grid(row=3, column=0)
-#ip_input = tk.Entry(main).grid(row=3, column=1)
 enter_button = tk.Button(main, text="Wake!", command=lambda: wake(mac_text.get())).grid(row=4, columnspan=2)
 arp_button = tk.Button(main, text="Display current ARP table", command=arp).grid(row=5, columnspan=2)
 
